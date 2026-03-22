f29d2f862e701aee50cc9bc67188d9c21952d561
neyser.huaman@moovecars.com <neyser.huaman@moovecars.com>
2026-03-22 23:59:26 +0100

    Initial commit

---
+++ src/main/java/com/neyser/appcompras/AppcomprasApplication.java
@@ -0,0 +1,13 @@
+package com.neyser.appcompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcomprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neyser/appcompras/AppcomprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neyser.appcompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcomprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
